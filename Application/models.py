4ab4569e7e137056f49cf74abad372fe1799e133
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-06-19 03:50:56 +0530

    Created database models and directory structure and iitialized app

---
+++ Application/models.py
@@ -0,0 +1,49 @@
+from .database import db
+
+# USER TABLE
+class User(db.Model):
+    __tablename__='user'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80),nullable=False)
+    email=db.Column(db.String(150),unique=True,nullable=False)
+    password=db.Column(db.String(512),nullable=False)
+    address=db.Column(db.String(250),nullable=False)
+    pincode=db.Column(db.String(6),nullable=False)
+    role=db.Column(db.String(10),default='user',nullable=False)
+
+    #relationship with records
+    record=db.relationship('Parkingrecords',backref='user',lazy=True)
+
+#Parking Lot Table
+class Parkinglot(db.Model):
+    __tablename__='parking_lot'
+    id=db.Column(db.Integer,primary_key=True)
+    prime_location_name=db.Column(db.String(80),nullable=False)
+    price=db.Column(db.Float,nullable=False)
+    address=db.Column(db.String(250),nullable=False)
+    pincode=db.Column(db.String(6),nullable=False)
+    maximum_spots=db.Column(db.Integer,nullable=False)
+    is_active=db.Column(db.Boolean,default=True,nullable=False)
+
+    #Relationship with parking spots
+    spots=db.relationship('Parkingspots',backref='lot',lazy=True)
+
+class Parkingspots(db.Model):
+    __tablename__='parking_spots'
+    id=db.Column(db.Integer,primary_key=True)
+    lot_id=db.Column(db.Integer,db.ForeignKey('parking_lot.id'),nullable=False)
+    spot_number=db.Column(db.Integer,nullable=False)
+    status=db.Column(db.String(1),default='A',nullable=False) # A= Available,O=Occupied
+
+    #relationship with records
+    record=db.relationship('Parkingrecords',backref='spot',lazy=True)
+
+class Parkingrecords(db.Model):
+    __tablename__='parking_records'
+    id=db.Column(db.Integer,primary_key=True)
+    spot_id=db.Column(db.Integer,db.ForeignKey('parking_spots.id'),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    parking_timestamp=db.Column(db.DateTime,nullable=False)
+    leaving_timestamp=db.Column(db.DateTime,nullable=True)
+    parking_cost=db.Column(db.Float,nullable=True)
+    status=db.Column(db.String(12),nullable=False,default='active') # active and released
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from Application.database import db
+app=None
+
+def create_app():
+    # global app
+    app=Flask(__name__)
+    app.debug=True
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///parkingapp.sqlite" 
+    db.init_app(app)
+
+    with app.app_context():
+        import Application.controllers
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
