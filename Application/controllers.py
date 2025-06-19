82bc66946a4f603c48f3004a855d0995df34454a
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-06-20 02:05:22 +0530

    Added home page using template inheritance

---
+++ Application/controllers.py
@@ -0,0 +1,7 @@
+from flask import Flask,render_template,redirect,request
+from flask import current_app as app
+from .models import * #inheriting models module to make indirect connection with app.py
+
+@app.route('/')
+def home():
+    return render_template('home.html')
