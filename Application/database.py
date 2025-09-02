82ded4309990ffe931fa4f3ade8c73e6ea3d2c81
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-09-02 09:28:09 +0530

    Added comments for the database.py file

--- Application/database.py
+++ Application/database.py
@@ -1,2 +1,6 @@
+# Imports the SQLAlchemy class from the flask_sqlalchemy library. This class is the main entry point for using the extension.
 from flask_sqlalchemy import SQLAlchemy
+# Creates an instance of the SQLAlchemy class. This object, conventionally named 'db', 
+# represents the database and provides access to all the functions and classes from SQLAlchemy,
+# like the Model class for defining database tables.
 db=SQLAlchemy()
